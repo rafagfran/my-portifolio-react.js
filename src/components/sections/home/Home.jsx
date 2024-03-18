3b98e8c61d81918a97d967c5fd0fbf57985b5708
Rafael G. Francisco <Rafaelgfra@hotmail.com>
2024-03-18 16:08:11 -0300

    Inseridos links de gitub e linkedin e adicionada bio copiada do linkedin

--- src/components/sections/home/Home.jsx
+++ src/components/sections/home/Home.jsx
@@ -31,15 +31,15 @@ function Home(){
             
             <div className={Style.content_box}>
                 <div className={Style.icon_box}>
-                    <i className='bx bxl-github'></i>
-                    <i className='bx bxl-linkedin-square' ></i>
+                    <a href="https://github.com/rafagfran"><i className='bx bxl-github'/></a>
+                    <a href="https://www.linkedin.com/in/rafael-g-francisco-90a886210/"><i className='bx bxl-linkedin-square' /></a>
                 </div>
                 <div className={Style.text_box}>
                     <h1>Hi, I'm Rafael Gonçalves</h1>
                     <h1>and i'm&nbsp;
                         <span ref={typedElementRef} className={Style.skills}>Backend developer</span>
                     </h1>
-                    <p>Quis incididunt consequat occaecat pariatur aliqua consequat ex. Do occaecat esse reprehenderit laboris cillum aliqua tempor deserunt mollit ipsum do velit. Esse et qui in dolor nisi. Adipisicing qui aute ex deserunt adipisicing enim.</p>
+                    <p>Atualmente graduando na FATEC-SJRP. Tenho três anos de experiência como profissional de TI e me dedico a aprimorar minhas habilidades no campo do desenvolvimento de aplicações, que é meu foco profissional.</p>
                     <button type='button' className={Style.btn}>Dowload CV</button>
                     
                 </div>
